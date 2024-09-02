4d6967d3744d4bc9deca333a97602533401d92a5
development <69533260+satyamde@users.noreply.github.com>
2024-09-02 23:48:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
         <title>PHP Test</title>
     </head>
     <body bgcolor="blue">
-        <?php echo '<H1><p>This domain is under control and maintained by Genpact OST</p><H1>'; ?>
+        <?php echo '<H1><p>This site is under control and maintained by Genpact OST</p><H1>'; ?>
     </body>
 </html>
