b56fc17914c89141b08fa28748b6a3dba0eaa411
development <69533260+satyamde@users.noreply.github.com>
2024-09-14 23:36:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>PHP Test</title>
+        <title>Maintenance</title>
     </head>
     <body bgcolor="blue">
         <?php echo '<H1><p>This site is under Maintenance</p><H1>'; ?>
